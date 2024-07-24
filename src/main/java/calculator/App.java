dc4a9217324b65f867f4f27dc99f4cbb45e1013d
Your Name <doolchong@gmail.com>
2024-07-24 14:40:16 +0900

    Level 1-8

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -54,6 +54,13 @@ public class App {
                 System.out.println(resultNumbers.poll() + "(이)가 삭제되었습니다.");
             }
 
+            //inquiry 입력 시 저장된 연산 결과 전부를 출력
+            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+            String inquiry = scanner.next();
+            if (inquiry.equals("inquiry")) {
+                System.out.println(resultNumbers);
+            }
+
             //exit을 입력 받으면 반복 종료
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String exit = scanner.next();
