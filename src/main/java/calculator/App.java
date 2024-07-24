af26773d1129cbd843f45ce420800dd74e5edf5a
Your Name <doolchong@gmail.com>
2024-07-24 14:35:44 +0900

    Level 1-7

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,15 +1,15 @@
 package calculator;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        //연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성
-        int[] resultNumbers = new int[10];
-        //연산의 결과가 저장된 배열의 마지막 index를 저장하는 변수를 선언
-        int index = 0;
+        //가장 먼저 저장된 연산 결과부터 삭제해야 하므로 Queue에 저장
+        Queue<Integer> resultNumbers = new LinkedList<>();
 
         //반복문 사용 해서 연산을 반복
         while (true) {
@@ -43,22 +43,21 @@ public class App {
             }
 
             System.out.println("결과: " + result);
-            //연산 결과가 10개를 초과하는 경우 가장 먼저 저장된 결과를 삭제하고 새로운 연산 결과가 저장
-            if (index >= resultNumbers.length) {
-                for (int i = 1; i < resultNumbers.length; i++) {
-                    resultNumbers[i - 1] = resultNumbers[i];
-                }
-                index--;
-            }
-            //연산의 결과를 배열에 저장
-            resultNumbers[index] = result;
-            //index를 증가
-            index++;
 
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            //연산의 결과를 Queue에 저장
+            resultNumbers.add(result);
+
+            //remove 입력 시 가장 먼저 저장된 연산 결과를 삭제
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            String remove = scanner.next();
+            if (remove.equals("remove")) {
+                System.out.println(resultNumbers.poll() + "(이)가 삭제되었습니다.");
+            }
+
             //exit을 입력 받으면 반복 종료
-            String end = scanner.next();
-            if (end.equals("exit")) {
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String exit = scanner.next();
+            if (exit.equals("exit")) {
                 break;
             }
         }
