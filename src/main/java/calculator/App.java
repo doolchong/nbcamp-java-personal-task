a7ec1cb29dc9a12477135c0e67d83e13e5812d45
Your Name <doolchong@gmail.com>
2024-07-24 13:16:21 +0900

    Level 1-3

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,6 +6,7 @@ public class App {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int result = 0;
 
         //Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장한다.
         System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -13,8 +14,27 @@ public class App {
         System.out.print("두 번째 숫자를 입력하세요: ");
         int num2 = scanner.nextInt();
 
-        System.out.println("사칙연산 기호를 입력하세요: "); // +, -, *, /
+        System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, /
         char operator = scanner.next().charAt(0);
 
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                } else {
+                    result = num1 / num2;
+                }
+        }
+
+        System.out.println("결과: " + result);
     }
 }
