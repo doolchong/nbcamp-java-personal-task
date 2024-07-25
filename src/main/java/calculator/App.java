b0e05abfaa777a52135c6a83fbf1cfd3ba4c706b
Your Name <doolchong@gmail.com>
2024-07-25 16:49:50 +0900

    Level 2-7

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,43 +1,67 @@
 package calculator;
 
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Calculator calculator = new Calculator(new LinkedList<>());
+        Calculator calculator = new Calculator();
 
         //반복문 사용 해서 연산을 반복
         while (true) {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int num1 = scanner.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = scanner.nextInt();
+            System.out.println("사칙연산 계산: 0, 원의 넓이 계산: 1");
+            System.out.print("어떤 계산을 하시겠습니까? ");
+            int mode = scanner.nextInt();
 
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char operator = scanner.next().charAt(0);
+            if (mode == 0) {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                int num1 = scanner.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                int num2 = scanner.nextInt();
 
-            //예외 처리용
-            try {
-                System.out.println("결과: " + calculator.calculate(num1, num2, operator));
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                char operator = scanner.next().charAt(0);
 
-            //remove 입력 시 가장 먼저 저장된 연산 결과를 삭제
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            String remove = scanner.next();
-            if (remove.equals("remove")) {
-                System.out.println(calculator.getter().poll() + "(이)가 삭제되었습니다.");
-            }
+                //예외 처리용
+                try {
+                    System.out.println("결과: " + calculator.calculate(num1, num2, operator));
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
 
-            //inquiry 입력 시 저장된 연산 결과 전부를 출력
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            String inquiry = scanner.next();
-            if (inquiry.equals("inquiry")) {
-                System.out.println(calculator.getter().toString());
+                //remove 입력 시 가장 먼저 저장된 연산 결과를 삭제
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                String remove = scanner.next();
+                if (remove.equals("remove")) {
+                    System.out.println(calculator.a_remove() + "(이)가 삭제되었습니다.");
+                }
+
+                //inquiry 입력 시 저장된 연산 결과 전부를 출력
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                String inquiry = scanner.next();
+                if (inquiry.equals("inquiry")) {
+                    System.out.println(calculator.a_inquiry());
+                }
+            } else if (mode == 1) {
+                System.out.print("원의 반지름을 입력하세요: ");
+                int radius = scanner.nextInt();
+
+                System.out.println("결과: " + calculator.calculateCircleArea(radius));
+
+                //remove 입력 시 가장 먼저 저장된 연산 결과를 삭제
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                String remove = scanner.next();
+                if (remove.equals("remove")) {
+                    System.out.println(calculator.c_remove() + "(이)가 삭제되었습니다.");
+                }
+
+                //inquiry 입력 시 저장된 연산 결과 전부를 출력
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                String inquiry = scanner.next();
+                if (inquiry.equals("inquiry")) {
+                    System.out.println(calculator.c_inquiry());
+                }
             }
 
             //exit을 입력 받으면 반복 종료
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,18 +1,24 @@
 package calculator;
 
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.regex.Pattern;
 
 public class Calculator {
     private static final String OPERATION_REG = "[+\\-*/]";
+    //원주율을 상수로서 활용
+    private static final double PI = 3.14159;
 
     //가장 먼저 저장된 연산 결과부터 삭제해야 하므로 Queue에 저장
-    private Queue<Integer> results;
+    private Queue<Integer> a_results;
+    private Queue<Double> c_results;
 
-    public Calculator(Queue<Integer> results) {
-        this.results = results;
+    public Calculator() {
+        a_results = new LinkedList<>();
+        c_results = new LinkedList<>();
     }
 
+    //사칙연산 메서드
     public int calculate(int num1, int num2, char operator) throws BadInputException {
         //연산자의 입력이 잘못된 경우
         if (!Pattern.matches(OPERATION_REG, String.valueOf(operator))) {
@@ -39,16 +45,46 @@ public class Calculator {
                 }
         }
         //연산의 결과를 Queue에 저장
-        results.add(result);
+        a_results.add(result);
 
         return result;
     }
 
-    public Queue<Integer> getter() {
-        return this.results;
+    public Queue<Integer> aGetter() {
+        return a_results;
     }
 
-    public void setter(Queue<Integer> queue) {
-        this.results = queue;
+    public void aSetter(Queue<Integer> queue) {
+        this.a_results = queue;
+    }
+
+    public int a_remove() {
+        return this.a_results.poll();
+    }
+
+    public String a_inquiry() {
+        return this.a_results.toString();
+    }
+
+    //원의 넓이를 구하는 메서드
+    public double calculateCircleArea(int radius) {
+        c_results.add(radius * radius * PI);
+        return radius * radius * PI;
+    }
+
+    public Queue<Double> cGetter() {
+        return c_results;
+    }
+
+    public void cSetter(Queue<Double> queue) {
+        this.c_results = queue;
+    }
+
+    public double c_remove() {
+        return this.c_results.poll();
+    }
+
+    public String c_inquiry() {
+        return this.c_results.toString();
     }
 }
