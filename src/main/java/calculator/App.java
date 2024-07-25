b5a663d3d8fb888447f49f1a3cf9d577be0e7866
Your Name <doolchong@gmail.com>
2024-07-25 14:45:16 +0900

    Level 2-2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,20 +1,15 @@
 package calculator;
 
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        //가장 먼저 저장된 연산 결과부터 삭제해야 하므로 Queue에 저장
-        Queue<Integer> resultNumbers = new LinkedList<>();
-
+        Calculator calculator = new Calculator();
+        
         //반복문 사용 해서 연산을 반복
         while (true) {
-            int result = 0;
-
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int num1 = scanner.nextInt();
             System.out.print("두 번째 숫자를 입력하세요: ");
@@ -23,42 +18,25 @@ public class App {
             System.out.print("사칙연산 기호를 입력하세요: ");
             char operator = scanner.next().charAt(0);
 
-            //switch문을 활용하여 사칙연산 기호에 맞는 연산 구현
-            switch (operator) {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    if (num2 == 0) {
-                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    } else {
-                        result = num1 / num2;
-                    }
+            //예외 처리용
+            try {
+                System.out.println("결과: " + calculator.calculate(num1, num2, operator));
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
-
-            System.out.println("결과: " + result);
-
-            //연산의 결과를 Queue에 저장
-            resultNumbers.add(result);
 
             //remove 입력 시 가장 먼저 저장된 연산 결과를 삭제
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             String remove = scanner.next();
             if (remove.equals("remove")) {
-                System.out.println(resultNumbers.poll() + "(이)가 삭제되었습니다.");
+                System.out.println(calculator.results.poll() + "(이)가 삭제되었습니다.");
             }
 
             //inquiry 입력 시 저장된 연산 결과 전부를 출력
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             String inquiry = scanner.next();
             if (inquiry.equals("inquiry")) {
-                System.out.println(resultNumbers);
+                System.out.println(calculator.results.toString());
             }
 
             //exit을 입력 받으면 반복 종료
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,34 +8,45 @@ public class Calculator {
     private static final String OPERATION_REG = "[+\\-*/]";
     private static final String NUMBER_REG = "^[0-9]*$";
 
+    //가장 먼저 저장된 연산 결과부터 삭제해야 하므로 Queue에 저장
     Queue<Integer> results = new LinkedList<>();
 
-    public void calculate(int num1, int num2, char operator) throws BadInputException {
+    public int calculate(int num1, int num2, char operator) throws BadInputException {
+        //연산자의 입력이 잘못된 경우
         if (!Pattern.matches(OPERATION_REG, String.valueOf(operator))) {
             throw new BadInputException("올바른 연산자를 입력해주세요.");
         }
+
+        //두 양의 정수의 입력이 잘못된 경우
         if (!Pattern.matches(NUMBER_REG, String.valueOf(num1))) {
             throw new BadInputException("올바른 정수값을 입력해주세요.");
         }
-        if (!Pattern.matches(OPERATION_REG, String.valueOf(num2))) {
+        if (!Pattern.matches(NUMBER_REG, String.valueOf(num2))) {
             throw new BadInputException("올바른 정수값을 입력해주세요.");
         }
+        int result = 0;
+
+        //연산자에 따른 사칙연산 구현
         switch (operator) {
             case '+':
-                results.add(num1 + num2);
+                result = num1 + num2;
                 break;
             case '-':
-                results.add(num1 - num2);
+                result = num1 - num2;
                 break;
             case '*':
-                results.add(num1 * num2);
+                result = num1 * num2;
                 break;
             case '/':
                 if (num2 == 0) {
                     throw new BadInputException("0으로 나눌 수 없습니다.");
                 } else {
-                    results.add(num1 / num2);
+                    result = num1 / num2;
                 }
         }
+        //연산의 결과를 Queue에 저장
+        results.add(result);
+
+        return result;
     }
 }
